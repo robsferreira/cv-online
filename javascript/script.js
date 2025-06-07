c137af530bd63fcd9fc01f51345d4943a97c783b
Robson Ferreira <robsferreira@gmail.com>
2025-06-07 02:37:28 -0300

    Atualizado cores header, footer

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,4 @@ const currentYear = new Date().getFullYear();
 // seleciona o elemento do rodapé
 const footer = document.querySelector('footer p');
 // atualliza o conteúdo do rodapé com o ano atual
-footer.innerHTML = `&copy; ${currentYear} Robson Ferreira. Todos os direitos reservados.`;
+footer.innerHTML = `&copy; ${currentYear} Robson Da Silva Ferreira. Todos os direitos reservados.`;
